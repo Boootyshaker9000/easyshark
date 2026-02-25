c554a3ba27531343bd8763eb83346c48a4ab137a
Boootyshaker9000 <relich@post.cz>
2026-02-25 18:52:54 +0100

    Corrected typos.

--- main.py
+++ main.py
@@ -136,7 +136,7 @@ def monitor_traffic():
     print(f"\n[*] Listening... (Filter: {user_filter if user_filter else 'All'} | Regime: {regime_text})")
 
     if count == 0:
-        print("[!] To shut down a return to the menu press Ctrl+C")
+        print("[!] To shut down and return to the menu press Ctrl+C")
 
     try:
         sniff(filter=user_filter, prn=packet_callback, count=count)
@@ -169,7 +169,7 @@ def check_arp_spoofing(packet):
 def run_arp_spoofing_check():
     print("\n--- Security check: ARP spoofing detection ---")
     print("[*] Listening and learning current network status...")
-    print("[!] To shut down a return to the menu press Ctrl+C")
+    print("[!] To shut down and return to the menu press Ctrl+C")
 
     known_arp_records.clear()
 
